b4686ab6adf0d145e59c2995b044dc9baa7b8075
ImagneRabbits <mixxdarkz2@gmail.com>
2019-09-26 01:20:50 +0700

    [ADD] implement speed up features !!!!

--- popup.js
+++ popup.js
@@ -1,10 +1,25 @@
-let changeColor = document.getElementById('changeColor');
-// ...
-changeColor.onclick = function(element) {
-  let color = element.target.value;
+let activeButton = document.getElementById('active');
+
+activeButton.onclick = function(element) {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    let SPEED = document.getElementById('speed').value;
+    let DISBLED_USELESS_BUTTON = document.getElementById('disbled-btn').checked;
+    const code = `
+      document.getElementsByTagName('video')[0].playbackRate = ${SPEED};
+
+      if (${DISBLED_USELESS_BUTTON}) {
+        var list = document.querySelectorAll('vg-cuepoint.ng-scope');
+        
+        for (let item of list) {
+            item.style.display = 'none';
+        }
+      }
+
+      console.log('speed up ${SPEED * 100}% !');
+    `
+
     chrome.tabs.executeScript(
         tabs[0].id,
-        {code: 'document.body.style.backgroundColor = "' + color + '";'});
+        {code});
   });
 };
