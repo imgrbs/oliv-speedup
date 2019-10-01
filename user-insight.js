10cc0d12a4f75988b8e9fbf620da5777bca4eb5b
ImagneRabbits <mixxdarkz2@gmail.com>
2019-10-01 16:41:30 +0700

    [UPDATE] replace to use google analytic and track event

--- user-insight.js
+++ user-insight.js
@@ -1,7 +1,40 @@
-var dataLayer = [];
-document.getElementById('active').addEventListener('click', function() {
-    dataLayer.push({
-    speed: document.getElementById('speed').value,
-    clearButton: document.getElementById('disbled-btn').checked
-    })
-})
+var _AnalyticsCode = 'UA-148718978-1';
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', _AnalyticsCode]);
+_gaq.push(['_trackPageview']);
+(function() {
+  var ga = document.createElement('script');
+  ga.type = 'text/javascript';
+  ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(ga, s);
+})();
+
+function trackButtonClick(e) {
+  _gaq.push(['_trackEvent', e.target.id, 'clicked']);
+}
+
+function trackSpeedClick(e) {
+  _gaq.push(['_trackEvent', e.target.value, 'selected']);
+}
+
+function trackCheckbox(e) {
+  _gaq.push(['_trackEvent', e.target.checked, 'clicked']);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  _gaq.push(['_trackEvent', 'extension open up', 'loaded']);
+
+  var speed = document.getElementById('speed');
+  speed.addEventListener('change', trackSpeedClick); 
+
+  var disableButton = document.getElementById('disbled-btn');
+  disableButton.addEventListener('change', trackCheckbox); 
+
+  var buttons = document.querySelectorAll('button');
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', trackButtonClick);
+  }
+});
