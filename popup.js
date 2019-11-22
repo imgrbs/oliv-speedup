93e14e70495d6bb3d828bfeeff65fd98356c102a
llUJO <jirapan_yankhan@hotmail.com>
2019-11-22 23:07:41 +0700

    [ EDIT ] Use quote

--- popup.js
+++ popup.js
@@ -4,64 +4,65 @@
 // Google Analytics
 // ****************** //
 
-var _AnalyticsCode = "UA-148718978-1";
+var _AnalyticsCode = 'UA-148718978-1';
 
 var _gaq = _gaq || [];
-_gaq.push(["_setAccount", _AnalyticsCode]);
-_gaq.push(["_trackPageview"]);
+_gaq.push(['_setAccount', _AnalyticsCode]);
+_gaq.push(['_trackPageview']);
 (function() {
-  var ga = document.createElement("script");
-  ga.type = "text/javascript";
+  var ga = document.createElement('script');
+  ga.type = 'text/javascript';
   ga.async = true;
-  ga.src = "https://ssl.google-analytics.com/ga.js";
-  var s = document.getElementsByTagName("script")[0];
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0];
   s.parentNode.insertBefore(ga, s);
 })();
 
 function trackButtonClick(e) {
-  _gaq.push(["_trackEvent", e.target.id, "clicked"]);
+  _gaq.push(['_trackEvent', e.target.id, 'clicked']);
 }
 
 function trackSpeedClick(e) {
-  _gaq.push(["_trackEvent", e.target.value, "selected"]);
-  document.getElementById("active").click();
+  _gaq.push(['_trackEvent', e.target.value, 'selected']);
+  document.getElementById('active').click();
 }
 
 function trackCheckbox(e) {
-  _gaq.push(["_trackEvent", e.target.checked, "clicked"]);
+  _gaq.push(['_trackEvent', e.target.checked, 'clicked']);
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-  _gaq.push(["_trackEvent", "extension open up", "loaded"]);
+document.addEventListener('DOMContentLoaded', function() {
+  _gaq.push(['_trackEvent', 'extension open up', 'loaded']);
 
-  var speed = document.getElementById("speed");
-  speed.addEventListener("change", trackSpeedClick);
+  var speed = document.getElementById('speed');
+  speed.addEventListener('change', trackSpeedClick);
 
-  var disableButton = document.getElementById("disbled-btn");
-  disableButton.addEventListener("change", trackCheckbox);
+  var disableButton = document.getElementById('disbled-btn');
+  disableButton.addEventListener('change', trackCheckbox);
 
-  var buttons = document.querySelectorAll("button");
+  var buttons = document.querySelectorAll('button');
   for (var i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", trackButtonClick);
+    buttons[i].addEventListener('click', trackButtonClick);
   }
 });
+
 
 // ****************** //
 // Core Features
 // ****************** //
 
-let activeButton = document.getElementById("active");
+let activeButton = document.getElementById('active');
 
 // Inject the script when start the tabs
 chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-  chrome.tabs.executeScript(tabs[0].id, { file: "script.js" });
+  chrome.tabs.executeScript(tabs[0].id, { file: 'script.js' });
 });
 
 // Handle when user clicks the `active` button
 activeButton.onclick = function(element) {
   chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-    let speed = document.getElementById("speed").value;
-    let isDisabledUselessButton = document.getElementById("disbled-btn")
+    let speed = document.getElementById('speed').value;
+    let isDisabledUselessButton = document.getElementById('disbled-btn')
       .checked;
 
     chrome.tabs.sendMessage(tabs[0].id, {
